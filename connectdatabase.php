c2e2e23b59d29ec1fb6c2cb02854d0ded59ca4db
Moon Soon Kim <56672937+anstn1993@users.noreply.github.com>
2019-10-23 18:14:22 +0900

    Update connectdatabase.php

--- connectdatabase.php
+++ connectdatabase.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
-$username = 'moonsoo'; # MySQL 계정 아이디
-$userpassword = 'Rla933466r!'; # MySQL 계정 패스워드
-$dbname = 'SNS';  # DATABASE 이름
+$user_name = 'user name'; # MySQL 계정 아이디
+$user_password = 'user password'; # MySQL 계정 패스워드
+$dbname = 'database name';  # DATABASE 이름
 
 //데이터베이스와 연결
 $conn = mysqli_connect($host, $username, $userpassword, $dbname);
