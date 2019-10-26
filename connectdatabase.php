85e9cff7e3d5d5f86fd2ddb73d65b5baaf19dc59
Moon Soon Kim <56672937+anstn1993@users.noreply.github.com>
2019-10-26 19:55:19 +0900

    Update connectdatabase.php

--- connectdatabase.php
+++ connectdatabase.php
@@ -1,7 +1,7 @@
 <?php
 $host = 'localhost';
-$user_name = 'user name'; # MySQL 계정 아이디(자신의 아이디로 수정해서 쓸 것)
-$user_password = 'user password'; # MySQL 계정 패스워드(자신의 비밀번호로 수정해서 쓸 것)
+$username = 'user name'; # MySQL 계정 아이디(자신의 아이디로 수정해서 쓸 것)
+$userpassword = 'user password'; # MySQL 계정 패스워드(자신의 비밀번호로 수정해서 쓸 것)
 $dbname = 'database name';  # DATABASE 이름(자신의 db이름으로 수정해서 쓸 )
 
 //데이터베이스와 연결
